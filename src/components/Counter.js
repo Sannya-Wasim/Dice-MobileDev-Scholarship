bc930e6300ee9cb673307f7ed491c7b745a74e7b
Sannya Wasim <sannya.wasim01@gmail.com>
2023-04-08 12:09:45 +0500

    Learned useEffect

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,7 +5,6 @@ import {
   Image,
   ScrollView,
   TouchableOpacity,
-  ActivityIndicator,
 } from "react-native";
 import { useState, useEffect } from "react";
 export const logo = require("../images/student.png");
@@ -13,42 +12,25 @@ export const default_image = require("../images/music.png");
 
 export default function Counter() {
   const [counter, setCounter] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
 
   //   const increaseCounter = () => {
   //     setCounter(counter + 1);
   //   };
 
-  // useEffect(() => {
-  //   alert("Counter Starting...");
-  // }, []);
+  useEffect(() => {
+    alert("Counter Starting...");
+  }, []);
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setCounter(counter + 1);
-  //   }, 3000);
-  // });
-
-  const stopLoading = () => {
-    if (isLoading == true) {
-      setIsLoading(false);
-    } else {
-      setIsLoading(true);
-    }
-  };
+  useEffect(() => {
+    setTimeout(() => {
+      setCounter(counter + 1);
+    }, 3000);
+  });
 
   return (
-    <View>
-      {/* <Text style={styles.text}>{counter}</Text>
-      <Text style={styles.text}>Increase Count</Text> */}
-      {isLoading == true ? (
-        <ActivityIndicator size="large" color="white"></ActivityIndicator>
-      ) : null}
-      <TouchableOpacity onPress={() => stopLoading()}>
-        <Text style={styles.text}>
-          {isLoading == true ? "Stop" : "Start"} Loading!
-        </Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <Text style={styles.text}>{counter}</Text>
+      <Text style={styles.text}>Increase Count</Text>
     </View>
   );
 }
@@ -57,7 +39,7 @@ const styles = StyleSheet.create({
   text: {
     color: "white",
     fontWeight: "bold",
-    fontSize: 20,
+    fontSize: 60,
     textAlign: "center",
   },
 });
